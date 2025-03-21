938c694e98aa7d8854fd88041e018e57753e7892
Arnaud Lyard <arnaud.lyard@gmail.com>
2025-03-21 19:51:59 +0100

    ⬆️ Upgrade dependency

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -24,8 +24,16 @@ export default async function RootLayout({
       className="text-zinc-950 antialiased dark:bg-zinc-900 dark:text-white dark:lg:bg-zinc-950"
     >
       <head>
-        <link rel="preconnect" href="https://rsms.me/" />
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        <link
+          rel="preconnect"
+          href="https://rsms.me/"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://rsms.me/inter/inter.css"
+          crossOrigin="anonymous"
+        />
       </head>
       <body>
         <Providers>
